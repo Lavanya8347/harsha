1bffdc1abfb4fe333e2cfef25f78d0c90d3a4647
Lavanya8347 <lavanya.e.mohan@gmail.com>
2024-07-13 10:52:41 +0530

    main project

---
+++ src/org/b/Emp.java
@@ -0,0 +1,33 @@
+package org.b;
+
+
+public class Emp {
+	private void empNmae() {
+		System.out.println("Lavanya");
+
+	}
+	private void empPhno() {
+		System.out.println("9876543210");
+
+	}
+	private void empLocation() {
+		System.out.println("Chennai");
+
+	}
+	public static void main(String[] args) {
+		Emp e = new Emp();
+		e.empNmae();
+		e.empPhno();
+		e.empLocation();
+	}
+	
+
+}
+
+
+
+
+
+
+
+
